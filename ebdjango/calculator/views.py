48b2d1f61628b13426a5448321be3d94894f4ce2
Siraj Hassan <sirajhassan@yahoo.com>
2020-05-30 21:38:36 -0500

    input works, needs better validation

--- ebdjango/calculator/views.py
+++ ebdjango/calculator/views.py
@@ -7,24 +7,30 @@ from .utils import calculate
 def calculator_view(request, *args, **kwargs):
 	quereyset = Calculation.objects.all() #list of all objects
 	form = CalculationForm(request.POST or None) #renders form is post data comes through or empty form 
-	
+	result = ''
 	#Deal with form and do calculation 
 	if form.is_valid():
+		print("1-------")
 		calculation = form.cleaned_data['calculation']
+		print("2-------")
 		result = calculate(calculation) #see utls
 		print(result)
 
 		if result == 'ERROR':
 			raise Exception('ERROR in input')
 		else:
+			print("3-------")
 			obj = form.save()
+			print("4-------")
 			obj.result = result #fill result field
 			form = CalculationForm()
+			print("5-------")
 
 	
 	context = { 
 		"form": form,
-		"calculations_list": quereyset
+		"calculations_list": quereyset,
+		"result" : result,
 	}
 
 	return render(request,"calculator/calculator_window.html",context)
