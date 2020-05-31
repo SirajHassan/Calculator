4c796a9f65c2c111e5dadd5586ad407210fbcb8d
Siraj Hassan <sirajhassan@yahoo.com>
2020-05-30 20:26:34 -0500

    model fields updated

--- ebdjango/calculator/models.py
+++ ebdjango/calculator/models.py
@@ -3,5 +3,6 @@ from django.db import models
 #store the strings and calculations generated as models 
 class Calculation(models.Model): 
 	calculation = models.TextField(blank = False, null = False) #store full calculation
-	
+	result= models.CharField(blank = False, null = False, max_length = 120) #store full calculation
+
 	
