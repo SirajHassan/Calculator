2b03a3b677734bba176a36c854777442691f632e
Siraj Hassan <sirajhassan@yahoo.com>
2020-05-31 11:19:32 -0500

    field name updated

--- ebdjango/calculator/models.py
+++ ebdjango/calculator/models.py
@@ -3,8 +3,8 @@ import time
 
 #store the strings and calculations generated as models 
 class Calculation(models.Model): 
-	calculation = models.TextField(blank = False, null = False) #store full calculation
-	result= models.CharField(blank = False, null = False, max_length = 120) #store full calculation
-	time = models.DecimalField(default = time.time(), blank = False, null = False, decimal_places = 0, max_digits = 10000000000000)
+	calc_input = models.TextField(blank = False, null = False) #inputted calculation
+	result= models.CharField(blank = False, null = False, max_length = 120) #calculations result
+	time = models.DecimalField(default = time.time(), blank = False, null = False, decimal_places = 0, max_digits = 10000000000000) #time of calculation
 
 	
