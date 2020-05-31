b282c1ea2e3d8cfffd085d0826a5dc481580ca5b
Siraj Hassan <sirajhassan@yahoo.com>
2020-05-30 20:26:02 -0500

    added URL for calculator and imported calculator view

--- ebdjango/ebdjango/urls.py
+++ ebdjango/ebdjango/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from calculator.views import calculator_view
 
 urlpatterns = [
+	path('',calculator_view,name='calculator'),
+	###admin####
     path('admin/', admin.site.urls),
 ]
