889524511fc76f1e84469533854cc6a1fd4b32c4
Siraj Hassan <sirajhassan@yahoo.com>
2020-05-30 21:38:56 -0500

    works with admin now

--- ebdjango/calculator/admin.py
+++ ebdjango/calculator/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Calculation #import product class from models 
 # Register your models here.
+
+admin.site.register(Calculation) #lets you manipualte on admin site 
+
+
