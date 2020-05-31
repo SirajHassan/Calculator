e31a10833e1dc88373ee9c21016ac055a3486ff9
Siraj Hassan <sirajhassan@yahoo.com>
2020-05-30 20:28:14 -0500

    view for calculator and list underway.

--- ebdjango/calculator/views.py
+++ ebdjango/calculator/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import Http404
+from .forms import CalculationForm
+from .models import Calculation
+from .utils import calculate
 
-# Create your views here.
+def calculator_view(request, *args, **kwargs):
+	quereyset = Product.objects.all() #list of all objects
+	form = CalculationForm(request.POST or None) #renders form is post data comes through or empty form 
+	
+	#Deal with form and do calculation 
+	if form.is_valid():
+		calculation = form.cleaned_data['calculation']
+		result = calculate(calculation) #see utls
+
+		if result == 'ERROR':
+			raise Exception('ERROR in input')
+		else:
+			obj = form.save()
+			obj.result = result #fill result field
+			form = CalculationForm()
+
+	
+	context = { 
+		"form": form,
+		"calculations_list": quereyset
+	}
+
+	return render(request,"calculator_window.html",context)
+
+
+
